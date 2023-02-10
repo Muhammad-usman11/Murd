741ed9d50d9fdc7af9a09d12f45904392393fc88
Muhammad <usmanehsanullah1@outlook.com>
2023-02-10 15:02:06 +0500

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 
 import { Inter } from '@next/font/google'
-import { Bottle } from './components/Bottle'
-import { Fire } from './components/Fire'
-import { Footer } from './components/Footer'
-import { FourImage } from './components/FourImage'
-import Navbar from './components/Navbar'
-import { Road } from './components/Road'
-import { Stone } from './components/Stone'
-import { Waste } from './components/Waste'
+import { Bottle } from '../components/Bottle'
+import { Fire } from '../components/Fire'
+import { Footer } from '../components/Footer'
+import { FourImage } from '../components/FourImage'
+import Navbar from '../components/Navbar'
+import { Road } from '../components/Road'
+import { Stone } from '../components/Stone'
+import { Waste } from '../components/Waste'
 
 
 const inter = Inter({ subsets: ['latin'] })
